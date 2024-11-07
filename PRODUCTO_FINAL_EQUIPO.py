ea737fd32fcfda8783253f9ddbb9e75e03509ec7
Fabricio Meneses Avila <133735106+FabMeneses@users.noreply.github.com>
2024-11-07 14:55:33 -0600

    JORGE

--- PRODUCTO_FINAL_EQUIPO.py
+++ PRODUCTO_FINAL_EQUIPO.py
@@ -15,8 +15,11 @@ background_label.place(relwidth=1, relheight=1)
 menu_bar = tk.Frame(root, bg="lightgrey")
 menu_bar.pack(side="top", fill="x")
 
-for i in range(1, 6):
-    button = tk.Button(menu_bar, text=f"Botón {i}", command=on_button_click)
+# Lista de nombres para los botones
+button_names = ["Hilos", "Sockets", "Semaforos", "Patrones", "Ayuda", "Salir"]
+
+for name in button_names:
+    button = tk.Button(menu_bar, text=name, command=on_button_click)
     button.pack(side="left", expand=True, fill="both")
 
 # Configurar el tamaño de la ventana
